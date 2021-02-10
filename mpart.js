e8f2b33798722d788fc12fcce354725eba75128b
jwjang2 <77949397+jwjang2@users.noreply.github.com>
2021-02-10 13:21:46 +0900

    nodeJS v2.0
    nodeJS study - 개체 사용

---
+++ mpart.js
@@ -0,0 +1,15 @@
+var M = {
+v : 'V',
+f : function () {
+	console.log('M function f()');
+	}
+}
+
+var J = {
+v : 'J',
+f : function () {
+	console.log('J function f()');
+	}
+}
+
+module.exports = M;
